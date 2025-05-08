250c90abac51777b012974e8ba8cd9c53dc7ac0a
leandroltfx <leandroltfbr@hotmail.com>
2025-05-07 21:28:01 -0300

    iniciando projeto

---
+++ src/test/java/com/ltf/financecontrol/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltf.financecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
