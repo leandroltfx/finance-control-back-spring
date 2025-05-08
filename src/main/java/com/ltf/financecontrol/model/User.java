e5267557b7078e44ad462f4c985cc7d4901bdc35
leandroltfx <leandroltfbr@hotmail.com>
2025-05-08 20:38:40 -0300

    configurando getters, setters e toString com Lombok

--- src/main/java/com/ltf/financecontrol/model/User.java
+++ src/main/java/com/ltf/financecontrol/model/User.java
@@ -5,8 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@ToString
 public class User {
 
     @Id
@@ -22,45 +29,4 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "id='" + id + '\'' +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
