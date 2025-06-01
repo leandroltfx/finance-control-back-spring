ef300158a049b3a02efb212a9fe5a72bfb310192
leandroltfx <leandroltfbr@hotmail.com>
2025-06-01 09:10:19 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/ltfx/finance_control_back_spring/FinanceControlBackSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.ltfx.finance_control_back_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceControlBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceControlBackSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ltfx/finance_control_back_spring/FinanceControlBackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ltfx.finance_control_back_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlBackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
